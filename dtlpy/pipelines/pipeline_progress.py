e2d7a940d75fc8443cf013ed576d2574cc525884
orshabtay <or@dataloop.ai>
2019-04-10 15:09:49 +0300

    docs

--- dtlpy/pipelines/pipeline_progress.py
+++
@@ -1,9 +0,0 @@
-class PipelineProgress(object):
-    def __init__(self):
-        self.message = ''
-        self.status = ''
-        self.stage_status = list()
-        self.stage_n_steps = list()
-        self.stage_i_step = list()
-        self.i_stage = None
-        self.n_stages = None
