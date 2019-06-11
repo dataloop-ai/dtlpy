98bd2b15945c23acc08826dfce4b685387fed2b5
orshabtay <or@dataloop.ai>
2019-06-11 15:24:37 +0300

    fixed example

--- dtlpy/examples/copy_folder.py
+++ dtlpy/examples/copy_folder.py
@@ -9,7 +9,9 @@ def main():
     project = dl.projects.get(project_name='FirstProject')
     dataset_from = project.datasets.get(dataset_name='FirstDataset')
     # ll items from folder
-    pages = dataset_from.items.list(query={'filenames': ['/source_folder']})
+    pages = dataset_from.items.list(query={'filenames': ['/source_folder'],  # take files from the directory only
+                                           'itemType': 'file'  # only files
+                                           })
 
     # TO post annotations to other item
     project = dl.projects.get(project_name='SecondProjects')
@@ -17,10 +19,9 @@ def main():
 
     for page in pages:
         for item in page:
-            if page.type == 'dir':
-                continue
-
-            # download item
+            # download item (without save to disk)
             buffer = item.download(save_locally=False)
             # upload item
-            success = dataset_to.items.update(filepath=buffer, remote_path='/destination_folder')
+            success = dataset_to.items.upload(filepath=buffer,
+                                              remote_path='/destination_folder',
+                                              uploaded_filename=item.name)
