5bca8866d57785c4ade47d287bfbf2dce12095ba
AharonDL <aharon@dataloop.ai>
2019-11-20 12:42:40 +0200

    v1.8.16

---
+++ build-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker build -t gcr.io/viewo-g/sdk:$1 -f ./Dockerfile --build-arg BITBUCKET_TAG=$1 .
+docker tag gcr.io/viewo-g/sdk:$1 gcr.io/viewo-g/sdk:latest
+
+docker push gcr.io/viewo-g/sdk:$1
+docker push gcr.io/viewo-g/sdk:latest
