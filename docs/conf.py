fe4765fdbf58ec0fbbad1f52e9ed58cfef2dd4d4
Mohammed Galia <mohammedgalia@MohammedG-MBP.lan>
2024-04-15 13:49:42 +0300

    new docs version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Dataloop Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.90.36'
+release = '1.91.3'
 
 add_module_names = False
 
